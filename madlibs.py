771ff5635fc5f7352b93d6ca5861735a9d3e979d
ArchanaSathiyaseelans <77286741+ArchanaSathiyaseelans@users.noreply.github.com>
2021-06-02 08:20:59 -0900

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+
+adj = input("Adjective: ")
+verb = input("Verb: ")
+Super_hero = input("Favourite Super Hero: ")
+
+mablib = f"Computer programming is so {adj},! it makes me enjoyable and happy all the Time \
+stay happy and energitic all the time {verb}, like {Super_hero}!"
+
+
+print(mablib)
